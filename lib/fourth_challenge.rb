6a971f32d20997061f72aca5fc842ccdc805a035
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-18 00:18:09 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,9 +5,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [{
-        :name => "",
-        :age => "",
-        :attitude => ""}, {
+        name => "",
+        age => "",
+        attitude => ""}, {
           name: "",
           age: "",
           attitude ""
@@ -20,9 +20,9 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends =>  [{
-        :name => "",
-        :age => "",
-        :attitude => ""}, {
+        name => "",
+        age => "",
+        attitude => ""}, {
           name: "",
           age: "",
           attitude ""
