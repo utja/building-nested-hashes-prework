10e5a64d4aeb8490b53424d8b19377b772371d62
Jon Ahn <ahn.jon.s@gmail.com>
2018-06-18 00:09:48 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,6 +1,6 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
+    :montague => {:patriarch => {}, :matriarch => {}, :hero => {}, :hero_friends => []
       #your key/value pairs here!
       }, 
     :capulet => {
